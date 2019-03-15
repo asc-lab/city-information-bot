f6748cb3eed1dace4e9f94484ad197b87216b97a
Robert Witkowski <robert.witkowski@altkom.pl>
2019-03-15 10:12:17 +0100

    Live coding session - starting point.

--- src/bot.ts
+++ src/bot.ts
@@ -12,36 +12,20 @@ import * as _ from 'underscore';
 import {isNullOrUndefined} from 'util';
 import {ITimeAndWeatherService} from './services/i-time-and-weather.service';
 import {TimeAndWeatherService} from './services/time-and-weather.service';
-import {MockTimeAndWeatherService} from './services/mock-time-and-weather.service';
 
 export class MyBot {
 
-    private static readonly DIALOG_STATE_PROPERTY = 'dialogState';
-    private static readonly PARENT_DIALOG_ID = 'cityInformationDialog';
-    private static readonly CHOOSE_CITY_DIALOG_ID = 'chooseCityPrompt';
-    private static readonly CITY_OPTIONS = ['Warsaw', 'London', 'New York', 'Berlin', 'Other'];
-    private static readonly TYPE_CITY_ID = 'typeCityPrompt';
-
     private readonly dialogState: StatePropertyAccessor;
     private dialogs: DialogSet;
-
-    private readonly timeAndWeatherService: ITimeAndWeatherService =
-        process.env.MOCK_BACKEND === 'true'
-            ? new MockTimeAndWeatherService()
-            : new TimeAndWeatherService();
+    private static readonly PARENT_DIALOG_ID = 'cityInformationDialog';
 
     constructor(private conversationState: ConversationState) {
-        this.dialogState = this.conversationState.createProperty(MyBot.DIALOG_STATE_PROPERTY);
+        this.dialogState = this.conversationState.createProperty('dialogState');
         this.dialogs = new DialogSet(this.dialogState);
-        this.dialogs.add(new ChoicePrompt(MyBot.CHOOSE_CITY_DIALOG_ID));
-        this.dialogs.add(new TextPrompt(MyBot.TYPE_CITY_ID));
-        this.dialogs.add(new WaterfallDialog(MyBot.PARENT_DIALOG_ID, [
-            this.sendCityChoice.bind(this),
-            this.reactToCityChoice.bind(this),
-            this.reactToManualCityChoice.bind(this)
-        ]));
+
     }
 
+    // --------------------- CODE GENERATED FROM YEOMAN WITH LITTLE CHANGES ------------------
     public onTurn = async (turnContext: TurnContext) => {
         switch (turnContext.activity.type) {
             case ActivityTypes.Message:
@@ -85,46 +69,6 @@ export class MyBot {
         }
     }
 
-    private async sendCityChoice(context: WaterfallStepContext) {
-        return await context.prompt
-            (MyBot.CHOOSE_CITY_DIALOG_ID, {
-                choices: MyBot.CITY_OPTIONS,
-                prompt: 'Please choose one of the most popular cities or choose "Other" if you ask about another.',
-                retryPrompt: 'You did not provide expected value, could you try again?'
-            });
-    }
-
-    private async reactToCityChoice(step: WaterfallStepContext) {
-        const selectedCity = step.result.value;
-        if (selectedCity === this.getLastFromCityOptions()) {
-            return await step.prompt(MyBot.TYPE_CITY_ID, 'Please type manually city name');
-        }
-        await this.sendTimeAndWeatherInformation(step.context, selectedCity);
-        return step.endDialog();
-    }
-
-    private async reactToManualCityChoice(step: WaterfallStepContext) {
-        const selectedCity = step.result;
-        await this.sendTimeAndWeatherInformation(step.context, selectedCity);
-        return step.endDialog();
-    }
-
-    private async sendTimeAndWeatherInformation(context: TurnContext, city: string) {
-        try {
-            const timeAndWeatherResponse = await this.timeAndWeatherService.getTimeAndWeatherFor(city);
-            const promises = [
-                context.sendActivity(`Time for ${city} is: ${timeAndWeatherResponse.time}`),
-                context.sendActivity(`Detailed weather information for  ${city} is: ${timeAndWeatherResponse.weather}`)
-            ];
-            return Promise.all(promises);
-        } catch (error) {
-            console.error(error);
-            context.sendActivity(`Something went wrong`);
-        }
-    }
-
-    private getLastFromCityOptions(): string {
-        return MyBot.CITY_OPTIONS[MyBot.CITY_OPTIONS.length - 1];
-    }
+    // --------------------- CODE GENERATED FROM YEOMAN WITH LITTLE CHANGES ------------------
 
 }
